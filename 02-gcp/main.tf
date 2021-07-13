02641e260054523b29d56fb1c3d9aabb40012e9b
fveas <fveas@sosnet.io>
2021-07-13 16:00:13 -0400

    Se agregan parámetros para disco de inicio

--- 02-gcp/main.tf
+++ 02-gcp/main.tf
@@ -8,8 +8,43 @@ provider "google" {
 
 ## Network
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "terraform-network-vpc"
 }
+
+resource "google_compute_subnetwork" "vpc_network" {
+  name          = "terraform-subnetwork"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_address" "internal_with_subnet_and_address" {
+  name         = "terraform-network-internal"
+  subnetwork   = google_compute_subnetwork.vpc_network.id
+  address_type = "INTERNAL"
+  address      = "10.0.0.2"
+  region       = var.region
+}
+
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+
+# resource "google_compute_disk" "default" {
+#   name  = "terraform-01-test"
+#   type  = "pd-ssd"
+#   zone  = var.zone
+#   image = var.image
+#   labels = {
+#     environment = "dev"
+#   }
+#   physical_block_size_bytes = 4096
+# }
+
+# resource "google_compute_attached_disk" "default" {
+#   disk     = google_compute_disk.default.id
+#   instance = google_compute_instance.vm_instance.id
+# }
 
 ## Firewall
 resource "google_compute_firewall" "default" {
@@ -20,7 +55,7 @@ resource "google_compute_firewall" "default" {
   }
   allow {
     protocol = "tcp"
-    ports    = ["22"]
+    ports    = ["22", "80" ,"443"]
   }
   target_tags = ["fw-services"]
 }
@@ -33,13 +68,21 @@ resource "google_compute_instance" "vm_instance" {
 
 boot_disk {
   initialize_params {
+  type  = "pd-ssd"
   image = var.image
+  size = 20
   }
 }
+
+# attached_disk {
+#   source      = google_compute_disk.default.id
+#   device_name = "terraform-01-disk"
+# }
 
 network_interface {
   network = google_compute_network.vpc_network.self_link
   access_config {
+    nat_ip = google_compute_address.static.address
   }
 }
 }
